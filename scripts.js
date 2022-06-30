eb19e8f611b232216aebbf76ebcf58779239b40f
Norival <norivaljunior.jf@gmail.com>
2022-06-29 23:06:05 -0300

    finalizando layout pagina 2

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 let quizzes;
-let conteudo= document.querySelector('.conteudo');
+let conteudo = document.querySelector('.conteudo');
 let idQuiz = [];
 let todosQuizes;
-
+let caixaPerguntastemplate = "";
 quizzesDoUsuario();
 obterQuizz();
 
@@ -13,7 +13,7 @@ function quizzesDoUsuario() {
     <button onclick="">Criar Quizz</button>
     </div><h2>Todos os Quizes</h2>
     <div class="quizes"></div>`
-   conteudo.innerHTML += `${botaoTemplate}`;
+    conteudo.innerHTML += `${botaoTemplate}`;
 
 }
 
@@ -29,7 +29,7 @@ function exibirQuizzes(resposta) {
     todosQuizes = document.querySelector('.quizes');
 
     for (let i = 0; i < quizzes.length; i++) {
-        idQuiz.push(quizzes[i].id) 
+        idQuiz.push(quizzes[i].id)
 
         const quizTemplate = `<div class = "caixa-quiz" onclick="localizarQuiz(${idQuiz[i]})">
         <div class="caixa-imagem">
@@ -37,30 +37,66 @@ function exibirQuizzes(resposta) {
         <div class="gradiente"></div></div>
         <div class="titulo">${quizzes[i].title}</div></div>
         `
-                
+
         todosQuizes.innerHTML += `${quizTemplate}`
     }
 
 }
 
 function localizarQuiz(id) {
-    todosQuizes.innerHTML = "";
-    botaoCriar.innerHTML = "";
-    document.querySelector('h2').innerHTML = "";
-    
-    const promise = axios.get (`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
+    conteudo.innerHTML = "";
+
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
     promise.then(abrirQuiz);
 
 }
-function abrirQuiz (resposta){
 
-    const quiz = resposta.data;
-    console.log(quiz);
+function abrirQuiz(response) {
+
+    const quiz = response.data;
+    const perguntas = quiz.questions;
+
+    /*  Gerando titulo do Quiz */
+
     const tituloQuiz = `<div class="titulo-quiz">
     <p>${quiz.title}</p>
     <img class="img-titulo" src="${quiz.image}" alt="">
-    <div class="sombra"></div></div>`
+    <div class="sombra"></div>
+   </div>`
 
-    document.querySelector('.quiz-titulo').innerHTML += tituloQuiz;
+    conteudo.innerHTML += tituloQuiz;
+
+
+    /* Gerando perguntas do quiz */
+
+    for (let i = 0; i < perguntas.length; i++) {
+
+        let resposta = perguntas[i].answers;
+
+        caixaPerguntastemplate += `<div class="caixa-pergunta">
+        <div class="pergunta">${perguntas[i].title}</div>`
+
+
+
+        for (let i = 0; i < resposta.length; i++) {
+            caixaPerguntastemplate += `<div class="caixa-respostas">
+            <img class = "img-resposta" src="${resposta[i].image}" alt="">
+            <div class="resposta"><p>${resposta[i].text}<p></div></div>
+            `
+
+        }
+        caixaPerguntastemplate += `</div>`
+        conteudo.innerHTML += caixaPerguntastemplate;
+        caixaPerguntastemplate = `<div class="caixa-pergunta">`
+    }
+
+
+}
+
+function embaralhar() {
+    return Math.random() - 0.5;
+}
+
+function escolherPerguntas() {
 
 }
