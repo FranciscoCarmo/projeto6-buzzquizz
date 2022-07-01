8506d9508e46441b8b49643dddc45191fddc8f90
Francisco Carmo <franciscoacarmo@gmail.com>
2022-07-01 15:10:41 -0300

    Integra a funcionalidade de criar quizzes e voltar pra home

--- scripts.js
+++ scripts.js
@@ -79,7 +79,7 @@ function abrirQuiz(response) {
     let resposta = perguntas[j].answers;
 
     caixaPerguntastemplate = `<div class="caixa-pergunta fechada">
-        <div class="pergunta" style = "background-color: ${perguntas[j].color}">
+        <div class="perguntaCaixa" style = "background-color: ${perguntas[j].color}">
         ${perguntas[j].title}</div>`;
 
     for (let k = 0; k < resposta.length; k++) {
